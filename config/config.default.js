db7ab228920e437e20131e1753dca3e5a9ba66c8
lisniuse <17560235@qq.com>
2018-10-08 16:58:34 +0800

    update.

--- config/config.default.js
+++ config/config.default.js
@@ -23,11 +23,15 @@ module.exports = appInfo => {
         },
     };
 
+
+
     config.mongoose = {
-        url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/sentence',
-        options: {
-            poolSize: 20,
-        },
+        client: {
+            url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/sentence',
+            options: {
+                poolSize: 20
+            }
+        }
     };
 
     // add your config here
@@ -39,15 +43,15 @@ module.exports = appInfo => {
     config.notfound = {
         pageUrl: '/view/404.html',
     };
-    
+
     //站点信息
-    config.Site = function() {
+    config.Site = function () {
         return {
             title: "句悦网 - 毫无保留的免费英语词汇表",
             description: "免费在线提供所有阶段的备考参考词汇表，并且提供翻译、例句和发音，方便进行单词背诵，可自由进行切换预览且支持手机电脑预览。",
             keywords: "英语词汇大全,英语四级词汇表,考研英语词汇表,英语六级词汇表,小学英语词汇表,雅思词汇表"
         }
     }
-    
+
     return config;
 };
