63d8dc7b149257c0bad6e505722450c1a43b7e6d
lisniuse <17560235@qq.com>
2018-10-15 17:05:47 +0800

    update.

--- app/controller/words.js
+++ app/controller/words.js
@@ -10,8 +10,25 @@ class WordsController extends Controller {
 
     async index() {
         const { ctx, service, config } = this;
+        const number = String(ctx.params.number).replace(".html", "");
 
-        let data = {};
+        let categoryRes = await service.category.findOne({
+            number: number
+        });
+
+        console.log(categoryRes);
+
+        let wordsRes = await service.word.find({
+            categoryIds: [ categoryRes._id ]
+        });
+    
+        console.log(wordsRes);
+
+        let data = {
+            number: ctx.params.number,
+            category: categoryRes,
+            words: wordsRes
+        };
 
         await ctx.render('/pages/words', data);
     }
